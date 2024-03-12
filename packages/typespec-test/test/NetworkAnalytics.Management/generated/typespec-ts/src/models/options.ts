f338316dab4ed5713318a4f80b9f636475b8af81
Jiao Di (MSFT) <80496810+v-jiaodi@users.noreply.github.com>
2024-03-12 14:30:29 +0800

    add networking mgmt smoke-test

---
+++ packages/typespec-test/test/NetworkAnalytics.Management/generated/typespec-ts/src/models/options.ts
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { OperationOptions } from "@azure-rest/core-client";
+
+export interface AssociationsInterfaceGetOptions extends OperationOptions {}
+
+export interface AssociationsInterfaceCreateOrUpdateOptions
+  extends OperationOptions {}
+
+export interface AssociationsInterfaceUpdateOptions extends OperationOptions {}
+
+export interface AssociationsInterfaceDeleteOperationOptions
+  extends OperationOptions {}
+
+export interface AssociationsInterfaceListByTrafficControllerOptions
+  extends OperationOptions {}
+
+export interface FrontendsInterfaceGetOptions extends OperationOptions {}
+
+export interface FrontendsInterfaceCreateOrUpdateOptions
+  extends OperationOptions {}
+
+export interface FrontendsInterfaceUpdateOptions extends OperationOptions {}
+
+export interface FrontendsInterfaceDeleteOperationOptions
+  extends OperationOptions {}
+
+export interface FrontendsInterfaceListByTrafficControllerOptions
+  extends OperationOptions {}
+
+export interface TrafficControllerInterfaceGetOptions
+  extends OperationOptions {}
+
+export interface TrafficControllerInterfaceCreateOrUpdateOptions
+  extends OperationOptions {}
+
+export interface TrafficControllerInterfaceUpdateOptions
+  extends OperationOptions {}
+
+export interface TrafficControllerInterfaceDeleteOperationOptions
+  extends OperationOptions {}
+
+export interface TrafficControllerInterfaceListByResourceGroupOptions
+  extends OperationOptions {}
+
+export interface TrafficControllerInterfaceListBySubscriptionOptions
+  extends OperationOptions {}
+
+export interface OperationsListOptions extends OperationOptions {}
